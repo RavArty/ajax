93d52196cae2b1e29622a91159b70caf3ec1e925
Ravshan <ravkart@gmail.com>
2020-01-17 14:54:49 -0800

    ajax promise

--- js/index.js
+++ js/index.js
@@ -1,12 +1,26 @@
 console.log('Hello from the JavaScript console!');
 
 // Your AJAX request here
-$.ajax({
-  url:
-    'http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=bcb83c4b54aee8418983c2aff3073b3b',
-  type: 'GET',
-  success: function(data) {
-    console.log('Your callback here!', data);
-  }
-});
+// $.ajax({
+//   url:
+//     'http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=bcb83c4b54aee8418983c2aff3073b3b',
+//   type: 'GET',
+//   success: function(data) {
+//     console.log('Your callback here!', data);
+//   }
+// });
 // Add another console log here, outside your AJAX request
+
+const fetchSuccess = data => console.log(data);
+const fetchError = err => console.log(err);
+
+const fetchData = () =>
+  $.ajax({
+    url:
+      'http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=bcb83c4b54aee8418983c2aff3073b3b'
+  });
+// Note the implicit return!
+
+fetchData()
+  .then(fetchSuccess)
+  .fail(fetchError);
