ba87a7a16c4c1313bfeb22f360973b8d91ac0835
Ravshan <ravkart@gmail.com>
2020-01-17 14:52:12 -0800

    ajax 1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
-console.log("Hello from the JavaScript console!");
+console.log('Hello from the JavaScript console!');
 
 // Your AJAX request here
-
+$.ajax({
+  url:
+    'http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=bcb83c4b54aee8418983c2aff3073b3b',
+  type: 'GET',
+  success: function(data) {
+    console.log('Your callback here!', data);
+  }
+});
 // Add another console log here, outside your AJAX request
